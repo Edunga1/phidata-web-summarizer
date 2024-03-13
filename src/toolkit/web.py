78699e7e6aefb2242f546dd80c664ce2b8db2c95
Alleb <alleb.park@bunjang.co.kr>
2024-03-13 17:18:00 +0900

    Update WebVisitor toolkit to specify function signature

--- src/toolkit/web.py
+++ src/toolkit/web.py
@@ -3,13 +3,19 @@ from requests import get
 
 
 class WebVisitor(Toolkit):
-    def get_html(self, url: str):
-        """Get the HTML of a webpage.
+    def __init__(self):
+        super().__init__(name="web-visitor")
+
+        self.register(self.get_html)
+
+    def get_html(self, url: str) -> str:
+        """Use this function to get the content of a webpage.
 
         Args:
             url (str): The URL of the webpage.
 
         Returns:
-            str: The HTML of the webpage.
+            The content of the webpage.
+            May contain HTML, CSS, JavaScript, etc.
         """
         return get(url).text
